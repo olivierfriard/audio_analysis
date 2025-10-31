26b388f1c4107c0d72830c17a7cd6d501ca2316e
Olivier Friard <olivier.friard@unito.it>
2025-10-31 12:18:47 +0100

    some changes

--- src/analisi_canti/main.py
+++ src/analisi_canti/main.py
@@ -33,8 +33,8 @@ from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction
 import librosa
 
-__version__ = "0.0.12"
-__version_date__ = "2025-05-06"
+__version__ = "0.0.13"
+__version_date__ = "2025-10-31"
 
 
 from .oscillogram import OscillogramWindow
